f35d4e74f2267da3ccdb379716d39259965a6b49
HayrunNisaBagirkan <>
2023-08-28 12:50:00 +0000

    fix problems

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,107 @@
+// Array of test data containing questions and answers
 const testData = [
-    {
-        id: 1,
-        question: "What is the closest planet to the Sun ?",
-        a: "Venus",
-        b: "Earth",
-        c: "Mars",
-        d: "Mercury",
-        answer: "d",
-    },
-    {
-        id: 2,
-        question: "What is a group of stars that form a pattern called ?",
-        a: "Galaxy",
-        b: "Nebula",
-        c: "Constellation",
-        d: "Meteor",
-        answer: "c",
-    },
-    {
-        id: 3,
-        question: "What is the natural satellite of Earth ?",
-        a: "Sun",
-        b: "Mars",
-        c: "Moon",
-        d: "Saturn",
-        answer: "c",
-    },
-    {
-        id: 4,
-        question: "What is the largest planet in our solar system ?",
-        a: "Venus",
-        b: "Mars",
-        c: "Saturn",
-        d: "Jupiter",
-        answer: "d",
-    },
-    {
-        id: 5,
-        question: "What is a meteor that reaches the Earth's surface called ?",
-        a: "Meteorite",
-        b: "Asteroid",
-        c: "Meteoroid",
-        d: "Comet",
-        answer: "a",
-    },
-    {
-        id: 6,
-        question:
-            "What is the process by which a star turns into a red giant and sheds its outer layers ?",
-        a: "Solar eclipse",
-        b: "Supernova",
-        c: "Stellar evolution",
-        d: "Nebulay",
-        answer: "d",
-    },
-    {
-        id: 7,
-        question:
-            "What is the region in space where gravity is so strong that nothing, not even light, can escape it ?",
-        a: "Nebula",
-        b: "Asteroid belt",
-        c: "Black hole",
-        d: "Quasar",
-        answer: "c",
-    },
-    {
-        id: 8,
-        question:
-            "What is the name of the spacecraft that landed the first humans on the Moon ?",
-        a: "Voyager",
-        b: "Apollo",
-        c: "Discovery",
-        d: "Galileoy",
-        answer: "b",
-    },
-    {
-        id: 9,
-        question:
-            "What is the phenomenon of a star exploding and becoming extremely bright for a short period called ?",
-        a: "Black hole",
-        b: "Eclipse",
-        c: "Nova",
-        d: "Supernova",
-        answer: "d",
-    },
-    {
-        id: 10,
-        question:
-            "What is the name of the dwarf planet located beyond Pluto in our solar system ?",
-        a: "Neptune",
-        b: "Eris",
-        c: "Saturn",
-        d: "Jupiter",
-        answer: "b",
-    },
+  {
+    id: 1,
+    question: "What is the closest planet to the Sun ?",
+    a: "Venus",
+    b: "Earth",
+    c: "Mars",
+    d: "Mercury",
+    answer: "d",
+  },
+  {
+    id: 2,
+    question: "What is a group of stars that form a pattern called ?",
+    a: "Galaxy",
+    b: "Nebula",
+    c: "Constellation",
+    d: "Meteor",
+    answer: "c",
+  },
+  {
+    id: 3,
+    question: "What is the natural satellite of Earth ?",
+    a: "Sun",
+    b: "Mars",
+    c: "Moon",
+    d: "Saturn",
+    answer: "c",
+  },
+  {
+    id: 4,
+    question: "What is the largest planet in our solar system ?",
+    a: "Venus",
+    b: "Mars",
+    c: "Saturn",
+    d: "Jupiter",
+    answer: "d",
+  },
+  {
+    id: 5,
+    question: "What is a meteor that reaches the Earth's surface called ?",
+    a: "Meteorite",
+    b: "Asteroid",
+    c: "Meteoroid",
+    d: "Comet",
+    answer: "a",
+  },
+  {
+    id: 6,
+    question:
+      "What is the process by which a star turns into a red giant and sheds its outer layers ?",
+    a: "Solar eclipse",
+    b: "Supernova",
+    c: "Stellar evolution",
+    d: "Nebulay",
+    answer: "d",
+  },
+  {
+    id: 7,
+    question:
+      "What is the region in space where gravity is so strong that nothing, not even light, can escape it ?",
+    a: "Nebula",
+    b: "Asteroid belt",
+    c: "Black hole",
+    d: "Quasar",
+    answer: "c",
+  },
+  {
+    id: 8,
+    question:
+      "What is the name of the spacecraft that landed the first humans on the Moon ?",
+    a: "Voyager",
+    b: "Apollo",
+    c: "Discovery",
+    d: "Galileoy",
+    answer: "b",
+  },
+  {
+    id: 9,
+    question:
+      "What is the phenomenon of a star exploding and becoming extremely bright for a short period called ?",
+    a: "Black hole",
+    b: "Eclipse",
+    c: "Nova",
+    d: "Supernova",
+    answer: "d",
+  },
+  {
+    id: 10,
+    question:
+      "What is the name of the dwarf planet located beyond Pluto in our solar system ?",
+    a: "Neptune",
+    b: "Eris",
+    c: "Saturn",
+    d: "Jupiter",
+    answer: "b",
+  },
 ];
 
+// Variables to track current question and score
 let currentTest = 0;
 let score = 0;
 
+// DOM elements used in the test
 const questionContainer = document.getElementById("question-container");
 const questionCounterText = document.getElementById("counter");
 const scoreText = document.getElementById("score");
@@ -113,57 +116,72 @@ const scoreResult = document.getElementById("score-result");
 const resultContainer = document.getElementById("result");
 const startAgainButton = document.getElementById("start-again-btn");
 
+// Function to start the test
+startGame = () => {
+  currentTest = 0;
+  score = 0;
+  scoreText.innerText = score;
+  loadTest();
+};
+
+// Function to deselect all answer choices
 const deselectAnswers = () => {
-    answerElements.forEach((answer) => (answer.checked = false));
+  answerElements.forEach((answer) => (answer.checked = false));
 };
 
+// Function to get the selected answer
 const getSelected = () => {
-    let answer;
-    answerElements.forEach((answerElement) => {
-        if (answerElement.checked) answer = answerElement.id;
-    });
-    return answer;
+  let answer;
+  answerElements.forEach((answerElement) => {
+    if (answerElement.checked) answer = answerElement.id;
+  });
+  return answer;
 };
 
+// Function to load and display the current test question
 const loadTest = () => {
-    deselectAnswers();
-    if (currentTest >= testData.length) {
-        resultContainer.style.display = "block";
-        scoreResult.innerText = score;
-        questionContainer.style.display = "none";
-        submitButton.style.display = "none";
-        startAgainButton.style.display = "block";
-
-    } else {
-        const currentTestData = testData[currentTest];
-        questionCounterText.innerText = `${currentTestData.id}/${testData.length}`;
-        scoreText.innerText = score;
-        questionElements.innerText = currentTestData.question;
-        a_button.innerText = currentTestData.a;
-        b_button.innerText = currentTestData.b;
-        c_button.innerText = currentTestData.c;
-        d_button.innerText = currentTestData.d;
-        resultContainer.style.display = "none";
-        questionContainer.style.display = "block";
-        submitButton.style.display = "block";
-        startAgainButton.style.display = "none";
-    }
+  deselectAnswers();
+  if (currentTest >= testData.length) {
+    // Display result when all questions are answered
+    resultContainer.style.display = "block";
+    scoreResult.innerText = score;
+    questionContainer.style.display = "none";
+    submitButton.style.display = "none";
+    startAgainButton.style.display = "block";
+  } else {
+    // Load and display current question and answer choices
+    const currentTestData = testData[currentTest];
+    questionCounterText.innerText = `${currentTestData.id}/${testData.length}`;
+    scoreText.innerText = score;
+    questionElements.innerText = currentTestData.question;
+    a_button.innerText = currentTestData.a;
+    b_button.innerText = currentTestData.b;
+    c_button.innerText = currentTestData.c;
+    d_button.innerText = currentTestData.d;
+    resultContainer.style.display = "none";
+    questionContainer.style.display = "block";
+    submitButton.style.display = "block";
+    startAgainButton.style.display = "none";
+  }
 };
 
+// Initial loading of the first question
 loadTest();
 
+// Event listener for submit button
 submitButton.addEventListener("click", () => {
-    const answer = getSelected();
-    if (answer) {
-        if (answer === testData[currentTest].answer) score++;
-        currentTest++;
-        if (currentTest < testData.length) loadTest();
-        else {
-            resultContainer.style.display = "block";
-            scoreResult.innerText = score;
-            questionContainer.style.display = "none";
-            submitButton.style.display = "none";
-            startAgainButton.style.display = "block";
-        }
+  const answer = getSelected();
+  if (answer) {
+    if (answer === testData[currentTest].answer) score++;
+    currentTest++;
+    if (currentTest < testData.length) loadTest();
+    else {
+      // Display final result when all questions are answered
+      resultContainer.style.display = "block";
+      scoreResult.innerText = score;
+      questionContainer.style.display = "none";
+      submitButton.style.display = "none";
+      startAgainButton.style.display = "block";
     }
+  }
 });
